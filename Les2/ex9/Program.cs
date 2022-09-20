66f8cccd9d5b41fff1921b42a9eca190d61ca5bc
Suraikin Vladislav <Ghost845JB@gmail.com>
2022-09-20 23:40:59 +0300

    Семинар 2 и папка для ДЗ

---
+++ Les2/ex9/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+int num1 = num / 10;
+int num2 = num % 10;
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
