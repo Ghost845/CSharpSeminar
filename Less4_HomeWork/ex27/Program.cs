54267d54643ccd69de797b5f50915b6fad9e7d46
Suraikin Vladislav <Ghost845JB@gmail.com>
2022-09-30 00:35:46 +0300

    ex27

---
+++ Less4_HomeWork/ex27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int temp = 0;
+while (a > 0)
+{
+    temp = a % 10;
+    sum += temp;
+    a = a / 10;
+}
+Console.WriteLine(sum);
