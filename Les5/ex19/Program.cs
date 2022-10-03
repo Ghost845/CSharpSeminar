e28f3cb155d373d5fbf212a7e88c0289c7889732
Suraikin Vladislav <Ghost845JB@gmail.com>
2022-10-03 21:49:24 +0300

    Les5 ex19

---
+++ Les5/ex19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int[] array = new int[5];
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int rev = 0;
+while (num > 0);
+{
+    rev = rev * 10 + num % 10;
+    num = num / 10;
+}
+if (rev == num)
+{
+    Console.WriteLine("Введённое число - палиндром");
+}
+else
+{
+    Console.WriteLine("Введённое число - не палиндром");
+}
