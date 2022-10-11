db33c18e3add95b727be464a41e6717e7616b107
Suraikin Vladislav <Ghost845JB@gmail.com>
2022-10-12 01:13:46 +0300

    ex54

---
+++ Less8_Homework/ex54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задан массив: ");
+int[,] newArr = Array(m, n);
+OrderArray(newArr);
+Console.WriteLine();
+WriteArray(newArr);
+
+
+int[,] Array(int m, int n)
+{
+    int[,] newArray = new int[m, n];
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            newArray[i, j] = new Random().Next(1, 10);
+            Console.Write($"{newArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    return newArray;
+}
+
+void OrderArray(int[,] array)
+{
+    for (int i = 0; i < newArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArr.GetLength(1); j++)
+        {
+            for (int k = 0; k < newArr.GetLength(1) - 1; k++)
+            {
+                if (newArr[i, k] < newArr[i, k + 1])
+                {
+                    int temp = newArr[i, k + 1];
+                    newArr[i, k + 1] = newArr[i, k];
+                    newArr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
---
+++ Less8_Homework/ex56/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+// Console.Clear();
+// Console.Write("Введите количество строк массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] Array(int m, int n)
+// {
+//     int[,] newArray = new int[m, n];
+//     for (int i = 0; i < newArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < newArray.GetLength(1); j++)
+//         {
+//             newArray[i, j] = new Random().Next(1, 10);
+//             Console.Write($"{newArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+
+//     return newArray;
+// }
+
