4a5436c03c76df1816ce2600d569bcb501af609a
Suraikin Vladislav <Ghost845JB@gmail.com>
2022-10-12 23:54:53 +0300

    ex58

---
+++ Less8_Homework/ex58/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+Console.Write("Введите количество строк первой матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов первой матрицы (и строк второй матрицы): ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов второй матрицы: ");
+int p = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] firstMatrix = new int[m, n];
+CreateArray(firstMatrix);
+Console.WriteLine($"Первая матрица: ");
+WriteArray(firstMatrix);
+
+int[,] secondMatrix = new int[n, p];
+CreateArray(secondMatrix);
+Console.WriteLine($"Вторая матрица: ");
+WriteArray(secondMatrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMatrix, secondMatrix, resultMatrix);
+Console.WriteLine($"Произведение матриц: ");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMatrix.GetLength(1); k++)
+      {
+        sum += firstMatrix[i,k] * secondMatrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
