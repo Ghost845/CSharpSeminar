7fe3dcaf64ab493ed1a91eefcd9c10926a8437b6
Suraikin Vladislav <Ghost845JB@gmail.com>
2022-10-18 22:39:19 +0300

    ex64

---
+++ Less9_Homework/ex64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.Write("Введите максимальное число для промежутка: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 0) Console.WriteLine("Число должно быть натуральным!");
+else
+{
+    int count = 2;
+    Print(n, count);
+    Console.Write(1);
+}
+void Print(int n, int count)
+{
+  if (count > n) return;
+  Print(n, count + 1);
+  Console.Write(count + ", ");
+}
